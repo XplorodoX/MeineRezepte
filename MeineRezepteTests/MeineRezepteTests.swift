c675b040ad338d03abe6a31715d1c69cf70dadd4
Merluee <merlaufl@gmail.com>
2025-07-03 15:30:17 +0200

    Initial Commit

---
+++ MeineRezepteTests/MeineRezepteTests.swift
@@ -0,0 +1,16 @@
+//
+//  MeineRezepteTests.swift
+//  MeineRezepteTests
+//
+//  Created by Florian Merlau on 03.07.25.
+//
+
+import Testing
+
+struct MeineRezepteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
